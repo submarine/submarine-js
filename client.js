715a1e8d4bf1c9512890b4240f5855436e4637dd
aparna <aparna.ashwin@discolabs.com>
2022-06-10 13:33:47 +1000

    SC27426 : remove js-sha256 package import

--- client.js
+++ client.js
@@ -1,4 +1,3 @@
-import { sha256 } from 'js-sha256';
 import { Submarine } from './src/submarine';
 
 // Define a customer, shop and API secret for use in development only.
@@ -8,28 +7,21 @@ import { Submarine } from './src/submarine';
 //
 // See https://hub.getsubmarine.com/docs/the-submarine-customer-api#authentication for how
 // Submarine Customer API signatures should be generated using the secret in a safe way.
+const DEV_ENVIRONMENT = 'dev';
 const DEV_CUSTOMER_ID = '5594588086341';
 const DEV_SHOP_DOMAIN = 'disco-aparna-sandbox.myshopify.com';
-
-// Calculate an API signature for the given customer, timestamp and API secret.
-// const calculateSignature = (customerId, timestamp, secret) => {
-//   const data = `${customerId}:${timestamp}`;
-//   return sha256.hmac(secret, data);
-// };
 
 // Generate a Submarine configuration object for use in API client development.
 //
 // See https://hub.getsubmarine.com/docs/the-submarine-customer-api#authentication for how
 // this configuration object should generally be generated within a Shopify theme.
 const generateDevConfiguration = () => {
-  const timestamp = new Date().getTime();
-
   return {
     authentication: {
       customer_id: DEV_CUSTOMER_ID,
-      shop: DEV_SHOP_DOMAIN,
+      shop: DEV_SHOP_DOMAIN
     },
-    environment: 'dev'
+    environment: DEV_ENVIRONMENT
   }
 };
 
