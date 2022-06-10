7e1cda6136d7046f0096d24ae8f7e8e4e20dbe6f
aparna <aparna.ashwin@discolabs.com>
2022-06-10 13:04:46 +1000

    SC27426 : remove timestamp and signature from authentication object

--- client.js
+++ client.js
@@ -8,16 +8,14 @@ import { Submarine } from './src/submarine';
 //
 // See https://hub.getsubmarine.com/docs/the-submarine-customer-api#authentication for how
 // Submarine Customer API signatures should be generated using the secret in a safe way.
-const DEV_ENVIRONMENT = 'dev';
 const DEV_CUSTOMER_ID = '5594588086341';
 const DEV_SHOP_DOMAIN = 'disco-aparna-sandbox.myshopify.com';
-const DEV_API_SECRET = 'npzfc1eVsGWCx6DwgjSNivH4';
 
 // Calculate an API signature for the given customer, timestamp and API secret.
-const calculateSignature = (customerId, timestamp, secret) => {
-  const data = `${customerId}:${timestamp}`;
-  return sha256.hmac(secret, data);
-};
+// const calculateSignature = (customerId, timestamp, secret) => {
+//   const data = `${customerId}:${timestamp}`;
+//   return sha256.hmac(secret, data);
+// };
 
 // Generate a Submarine configuration object for use in API client development.
 //
@@ -30,8 +28,6 @@ const generateDevConfiguration = () => {
     authentication: {
       customer_id: DEV_CUSTOMER_ID,
       shop: DEV_SHOP_DOMAIN,
-      signature: calculateSignature(DEV_CUSTOMER_ID, timestamp, DEV_API_SECRET),
-      timestamp: timestamp
     },
     environment: 'dev'
   }
